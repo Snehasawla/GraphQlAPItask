67e8ec2ab324533f2aa8314d3f17fa2cfe5ac83d
Sneha Sawla <snehasawla2409@gmail.com>
2021-05-03 17:48:17 +0530

    Login Useing Cookies

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,56 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import Express from "express";
-import { buildSchema, Resolver ,Query, formatArgumentValidationError } from "type-graphql";
+import { buildSchema, formatArgumentValidationError } from "type-graphql";
 import { createConnection } from "typeorm";
+import {RegisterResolver} from "./modules/Users/Register";
+import session from "express-session";
+import ConnectRedis from "connect-redis";
+import redis from "./redis";
+import cors from "cors";
+import { LoginResolver } from "./modules/Users/login";
+import {MeResolver} from "./modules/Users/Me";
 
-@Resolver()
-class HelloResolver{
-    @Query(() => String)
-    async helloword() {
-        return "Hello World";
-    }
-}
 
 const main = async () =>{
     await createConnection();
 
     const schema = await buildSchema({
-        resolvers: [ HelloResolver ]
+        resolvers: [ MeResolver, RegisterResolver, LoginResolver]
     });
 
-    const apolloServer = new ApolloServer({ schema, formatError: formatArgumentValidationError });
+    const apolloServer = new ApolloServer({ schema, 
+        formatError: formatArgumentValidationError,
+        context: ({req}: any) => ({ req }) 
+    });
     
     const app = Express();
+
+    const RedisStore = ConnectRedis(session);
+
+    app.use(
+        cors({
+            credentials:true,
+            origin: "http://localhost:3000"
+        })
+    )
+
+    app.use(
+        session({
+            store: new RedisStore({
+                client: redis
+            }),
+            name: "cookies",
+            secret:"create to complete task after 4 commit.",
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                maxAge: 1000*60*60*24*7*365// 7years of life
+            } 
+        })
+    )
 
     apolloServer.applyMiddleware({ app });
 
